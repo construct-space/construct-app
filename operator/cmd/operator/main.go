adc6d0cbd14e4a1863c3cd2a40bf453eb6ff0836
Flakerim Ismani <flakerimi@gmail.com>
2026-03-24 02:46:57 +0100

    Release 0.6.4

--- operator/cmd/operator/main.go
+++ operator/cmd/operator/main.go
@@ -37,7 +37,7 @@ import (
 	"construct-operator/internal/vibe"
 )
 
-const Version = "0.6.3"
+const Version = "0.6.4"
 
 func providerFromSetting(settingKey, value string) provider.Provider {
 	if value == "" {
