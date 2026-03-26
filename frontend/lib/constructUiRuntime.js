efd7acedbca8dd06a4c76b3ea56b8fe0cf375e8f
Flakerim Ismani <flakerimi@gmail.com>
2026-03-26 03:40:19 +0100

    Use npm dist for UI runtime instead of local source path

--- frontend/lib/constructUiRuntime.js
+++ frontend/lib/constructUiRuntime.js
@@ -1,5 +1,5 @@
-export { default } from '../../../packages/ui/src/index.ts'
-export * from '../../../packages/ui/src/index.ts'
+export { default } from '../../node_modules/@construct-space/ui/dist/construct-ui.js'
+export * from '../../node_modules/@construct-space/ui/dist/construct-ui.js'
 
 export { default as ConfirmationModal } from '@/components/common/ConfirmationModal.vue'
 export { default as SplitPane } from '@/components/panels/SplitPane.vue'
